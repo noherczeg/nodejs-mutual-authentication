b0aff54ff5bd38ea5409483fee7ff98b7206e116
Norbert Herczeg <noherczeg@gmail.com>
2020-10-21 02:28:24 +0200

    upgrade Node API-s used

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
-var fs = require('fs'); 
-var https = require('https');
+const fs = require('fs');
+const https = require('https');
 
-var options = { 
-    key: fs.readFileSync('server/server-key.pem'), 
-    cert: fs.readFileSync('server/server-crt.pem'), 
-    ca: fs.readFileSync('ca/ca-crt.pem'), 
-    crl: fs.readFileSync('ca/ca-crl.pem'), 
-    requestCert: true, 
+const options = {
+    key: fs.readFileSync('server/server-key.pem'),
+    cert: fs.readFileSync('server/server-crt.pem'),
+    ca: fs.readFileSync('ca/ca-crt.pem'),
+    crl: fs.readFileSync('ca/ca-crl.pem'),
+    requestCert: true,
     rejectUnauthorized: true,
 };
 
 https
-    .createServer(options, function (req, res) { 
-        console.log(new Date()+' '+ 
-            req.connection.remoteAddress+' '+ 
-            req.socket.getPeerCertificate().subject.CN+' '+ 
-            req.method+' '+req.url); 
-        res.writeHead(200); 
-        res.end("hello world\n"); 
+    .createServer(options, (req, res) => {
+        const { connection, socket, method } = req;
+        console.log(`${new Date()} ${connection.remoteAddress} ${socket.getPeerCertificate().subject.CN} ${method} ${req.url}`);
+        res.writeHead(200);
+        res.end('hello world\n');
     })
     .listen(4433);
